b497e780c6a6a58e8386652f193ce4d666537904
perez1187 <o.perez1187@gmail.com>
2024-01-31 23:56:13 +0100

    refirect

--- src/services/userServices.jsx
+++ src/services/userServices.jsx
@@ -1,11 +1,11 @@
 
 // const apiUrl = 'http://127.0.0.1:8000/'
-const apiUrl =  'http://dsgdev2loadbalancer-1901974022.eu-central-1.elb.amazonaws.com/'
+const api =  'http://dsgdev2loadbalancer-1901974022.eu-central-1.elb.amazonaws.com/api/'
 
 //login function
 export function auth(credentials) {
     
-    return fetch(`${apiUrl}api/v1/auth/jwt/create/`, {
+    return fetch(`${api}v1/auth/jwt/create/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
